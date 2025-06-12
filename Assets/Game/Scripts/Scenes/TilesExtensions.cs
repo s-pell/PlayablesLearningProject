26c1855862f235da8ecc1da1be63f6c30872fd6b
spell <blaginina.job@gmail.com>
2025-06-12 18:59:26 +0500

    Tiles extensions

---
+++ Assets/Game/Scripts/Scenes/TilesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Game
+{
+    public static class TilesExtensions
+    {
+        public static void Deconstruct(this Vector2Int vect, out int x, out int y)
+        {
+            x = vect.x;
+            y = vect.y;
+        }
+
+        public static void Release(this GameObject tile)
+        {
+            Addressables.ReleaseInstance(tile);
+            Debug.Log("Префаб выгружен");
+        }
+    }
+}
